5cbe2f63d869e00d8186990d009ae0f0de2d91b5
munishconceptbiu <munishwaran.m@conceptbiu.com>
2022-08-22 13:13:14 +0530

    check db connection

--- api/_helpers/db.js
+++ api/_helpers/db.js
@@ -9,20 +9,28 @@ initialize();
 async function initialize() {
     // create db if it doesn't already exist
     const { host, port, user, password, database } = config.database;
-    const connection = await mysql.createConnection({ host, port, user, password });
+    // const connection = await mysql.createConnection({ host, port, user, password });
+    // const connection = await mysql.createConnection({host: host, user: user, database: database});
+    const connection = await mysql.createConnection({
+        host: '192.168.0.175',
+        user: 'testbiu',
+        password: 'C0ncept@!#',
+        database: 'qualitative_biu'
+      });
+
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
     const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
 
     // init models and add them to the exported db object
-    db.User = require('../users/user.model')(sequelize);
-    db.Artical = require('../artical/artical.model')(sequelize);
-    db.QaData = require('../artical/qa_data.model')(sequelize);
-    db.QaDataProduct = require('../artical/qa_data_product.model')(sequelize);
-    db.QaClientProduct = require('../artical/qa_client_product.model')(sequelize);
-    db.QaDataSpokesPerson = require('../artical/qa_data_spokesperson')(sequelize);
-    db.QaSpokesPerson = require('../artical/qa_spokesperson.model')(sequelize);
+    // db.User = require('../users/user.model')(sequelize);
+    // db.Artical = require('../artical/artical.model')(sequelize);
+    // db.QaData = require('../artical/qa_data.model')(sequelize);
+    // db.QaDataProduct = require('../artical/qa_data_product.model')(sequelize);
+    // db.QaClientProduct = require('../artical/qa_client_product.model')(sequelize);
+    // db.QaDataSpokesPerson = require('../artical/qa_data_spokesperson')(sequelize);
+    // db.QaSpokesPerson = require('../artical/qa_spokesperson.model')(sequelize);
 
     
     // sync all models with database
