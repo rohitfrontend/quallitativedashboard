770410bd2a9cacc287ef65e4ee84673276754b13
mamunish <munish.ciet.itl22@gmail.com>
2022-10-21 17:13:40 +0530

    error

--- api/artical/artical.controller.js
+++ api/artical/artical.controller.js
@@ -9,8 +9,8 @@ const storageCustom = require('./customStorageEngine')
 
 var XLSX = require('xlsx');
 const states = [
-    { id:0, city: "National", state: "National"},
-    { id:99, city: "Online Web", state:"Online Web"},
+    { id: 0, city: "National", state: "National" },
+    { id: 99, city: "Online Web", state: "Online Web" },
     { id: 28, city: "Ludhiana", state: "Punjab" },
     { id: 6, city: "Chandigarh", state: "Punjab" },
     { id: 10, city: "New Delhi", state: "New Delhi" },
@@ -34,12 +34,12 @@ const states = [
     { id: 35, city: "Kolkata", state: "West Bengal" },
     { id: 21, city: "Mumbai", state: "Maharashtra" },
     { id: 16, city: "Ranchi", state: "Jharkhand" },
-    { id: 12, city: "Vadodara", state: "Gujarat"},
-    { id: 12, city: "Rajkot", state: "Gujarat"},
-    { id: 17, city:"Mangalore", state: "Karnataka"},
-    { id:12, city:"Surat", state: "Gujarat"},
+    { id: 12, city: "Vadodara", state: "Gujarat" },
+    { id: 12, city: "Rajkot", state: "Gujarat" },
+    { id: 17, city: "Mangalore", state: "Karnataka" },
+    { id: 12, city: "Surat", state: "Gujarat" },
     { id: 21, city: "Nagpur", state: "Maharashtra" },
-    { id: 100, city: "Goa", state: "Goa"},
+    { id: 100, city: "Goa", state: "Goa" },
     { id: 33, city: "Greater Noida", state: "Uttar Pradesh" },
     { id: 33, city: "Noida", state: "Uttar Pradesh" },
 ]
@@ -47,7 +47,7 @@ const states = [
 exports.getArtical = async function (req, res, next) {
     let edition = await articalService.getEdition('Online Web');
     res.json({ edition: edition, message: 'Edition successful' })
-}   
+}
 exports.getList = async function (req, res, next) {
     articalService.getAllListUpload().then((data) => {
         res.json({ data: data, message: 'Artical successful' })
@@ -134,93 +134,93 @@ exports.saveArtical = async function (req, res, next) {
         const art = parseInt(e['article id']);
         if (art !== 0 && !isNaN(art)) {
 
-            articalService.getAll(req.body.client_id, e['article id'], e['company name'], e['media type']).then( async(dbdata) => {
+            articalService.getAll(req.body.client_id, e['article id'], e['company name'], e['media type']).then(async (dbdata) => {
                 const state_name = states.filter(state => state.city === e['edition']);
-                const edition =  e['media type'] === "Print" ? await articalService.getEdition(e['edition']) : {id: null};
+                const edition = e['media type'] === "Print" ? await articalService.getEdition(e['edition']) : { id: null };
                 qa_data = {
-                        
-                        state_name: state_name[0].state,              
-                        article_id: e['article id'],
-                        client_id: req.body.client_id,
-                        media_type: e['media type'],
-                        photo_mention: e['photo'],
-                        headline: e['headline'],
-                        headline_mention: e['headline mention'],
-                        prominence: e['prominence'],
-                        tonality: e['tonality'],
-                        vertical: e['vertical'],
-                        EV: e['ev'],
-                        theme: e['theme'],
-                        keyword_level_1: e['keyword_level_1'],
-                        Topic: e['topic'],
-                        publication_type: e['publication type'],
-                        publication: e['publication'],
-                        language: e['language'],
-                        category_A: e['category'],
-                        visibility_score: e['visibility score'],
-                        circulation_web_weightage: e["cir ('000) & web wtg"],
-                        co_score: e['co score'],
-                        edition: e['edition'],
-                        publish_date: moment(e['publish date']).format('YYYY-MM-DD'),
-                        mav: e['mav'],
-                        ccm: e['ccm'],
-                        word_count: e['word count'],
-                        press_release: e['press release'],
-                        page_no: e['page no'],
-                        circlation: e['circulation'],
-                        zone: e['zone'],
-                        link: e['undefined'],
-                        website_url: e['undefined'],
-                        month_name: e['month'],
-                        monthly_visits: e['monthly visitors*'],
-                        total_CCMs: e['total ccms'],
-                        client_article_type: e['article type'],
-                        photo_weightage: e['photo weightage'],
-                        headline_weightage: e['headline weightage'],
-                        prominence_weightage: e['prominence weightage'],
-                        word_count_weightage: e['word count wtg'],
-                        front_Page_weightage: e['front page'],
-                        index_weightage: e['index'],
-                        source_name: e['journalist'],
-                        entity_name: e['company name'],
-                        prominence: e['prominence'],
-                        client_name : req.body.client_name,
-                        edition_id: edition?.id
-                    }
-                if(e['media type'] === 'Print'){
+
+                    state_name: state_name[0].state,
+                    article_id: e['article id'],
+                    client_id: req.body.client_id,
+                    media_type: e['media type'],
+                    photo_mention: e['photo'],
+                    headline: e['headline'],
+                    headline_mention: e['headline mention'],
+                    prominence: e['prominence'],
+                    tonality: e['tonality'],
+                    vertical: e['vertical'],
+                    EV: e['ev'],
+                    theme: e['theme'],
+                    keyword_level_1: e['keyword_level_1'],
+                    Topic: e['topic'],
+                    publication_type: e['publication type'],
+                    publication: e['publication'],
+                    language: e['language'],
+                    category_A: e['category'],
+                    visibility_score: e['visibility score'],
+                    circulation_web_weightage: e["cir ('000) & web wtg"],
+                    co_score: e['co score'],
+                    edition: e['edition'],
+                    publish_date: moment(e['publish date']).format('YYYY-MM-DD'),
+                    mav: e['mav'],
+                    ccm: e['ccm'],
+                    word_count: e['word count'],
+                    press_release: e['press release'],
+                    page_no: e['page no'],
+                    circlation: e['circulation'],
+                    zone: e['zone'],
+                    link: e['undefined'],
+                    website_url: e['undefined'],
+                    month_name: e['month'],
+                    monthly_visits: e['monthly visitors*'],
+                    total_CCMs: e['total ccms'],
+                    client_article_type: e['article type'],
+                    photo_weightage: e['photo weightage'],
+                    headline_weightage: e['headline weightage'],
+                    prominence_weightage: e['prominence weightage'],
+                    word_count_weightage: e['word count wtg'],
+                    front_Page_weightage: e['front page'],
+                    index_weightage: e['index'],
+                    source_name: e['journalist'],
+                    entity_name: e['company name'],
+                    prominence: e['prominence'],
+                    client_name: req.body.client_name,
+                    edition_id: edition?.id
+                }
+                if (e['media type'] === 'Print') {
                     const [print_data, created] = dbdata;
-                    if(print_data){
-                    qa_data.publication_id = print_data.dataValues.publication_id;
-                    // qa_data.edition_id = print_data.dataValues.edition_id,
-                    qa_data.publication_type_id = print_data.dataValues.publication_type_id
-                    qa_data.language_id = print_data.dataValues.language_id
-                    qa_data.suppliment_id= print_data.dataValues.suppliment_id,
-                    qa_data.source_id = print_data.dataValues.source_id
-                    qa_data.cav_id = print_data.dataValues.cav_id
-                    qa_data.entity_id = print_data.dataValues.entity_id
-                    qa_data.zone_id = print_data.dataValues.zone_id
-                    qa_data.prominent_id = print_data.dataValues.prominent_id
-                    qa_data.section_id = print_data.dataValues.section_id
+                    if (print_data) {
+                        qa_data.publication_id = print_data.dataValues.publication_id;
+                        // qa_data.edition_id = print_data.dataValues.edition_id,
+                        qa_data.publication_type_id = print_data.dataValues.publication_type_id
+                        qa_data.language_id = print_data.dataValues.language_id
+                        qa_data.suppliment_id = print_data.dataValues.suppliment_id,
+                            qa_data.source_id = print_data.dataValues.source_id
+                        qa_data.cav_id = print_data.dataValues.cav_id
+                        qa_data.entity_id = print_data.dataValues.entity_id
+                        qa_data.zone_id = print_data.dataValues.zone_id
+                        qa_data.prominent_id = print_data.dataValues.prominent_id
+                        qa_data.section_id = print_data.dataValues.section_id
                     }
-                    else{
+                    else {
 
                     }
                 }
-                if(e['media type'] === 'Online'){
-                   const [online_data, created] = dbdata;
-                   if(online_data) {
-                   qa_data.publication_id = online_data.dataValues.publication_id;
-                    // qa_data.edition_id = online_data.dataValues.edition_id,
-                    qa_data.publication_type_id = online_data.dataValues.publication_type_id
-                    qa_data.language_id = online_data.dataValues.language_id
-                    qa_data.suppliment_id= online_data.dataValues.suppliment_id,
-                    qa_data.source_id = online_data.dataValues.source_id
-                    qa_data.cav_id = online_data.dataValues.cav_id
-                    qa_data.entity_id = online_data.dataValues.entity_id
-                    qa_data.zone_id = online_data.dataValues.zone_id
-                    qa_data.prominent_id = online_data.dataValues.prominent_id
-                    qa_data.section_id = online_data.dataValues.section_id
-                   }
+                if (e['media type'] === 'Online') {
+                    const [online_data, created] = dbdata;
+                    if (online_data) {
+                        qa_data.publication_id = online_data.dataValues.publication_id;
+                        // qa_data.edition_id = online_data.dataValues.edition_id,
+                        qa_data.publication_type_id = online_data.dataValues.publication_type_id
+                        qa_data.language_id = online_data.dataValues.language_id
+                        qa_data.suppliment_id = online_data.dataValues.suppliment_id,
+                            qa_data.source_id = online_data.dataValues.source_id
+                        qa_data.cav_id = online_data.dataValues.cav_id
+                        qa_data.entity_id = online_data.dataValues.entity_id
+                        qa_data.zone_id = online_data.dataValues.zone_id
+                        qa_data.prominent_id = online_data.dataValues.prominent_id
+                        qa_data.section_id = online_data.dataValues.section_id
+                    }
                 }
                 // console.log('qa_data', qa_data)
                 await articalService.createQaData(qa_data).then(async (q_articles) => {
@@ -229,7 +229,7 @@ exports.saveArtical = async function (req, res, next) {
                     // if(created === false) {
                     //     await articalService.updateQaData(qa_data, q_articles)
                     // }
-                    if(q_articles){
+                    if (q_articles) {
                         const spokesman = Object.entries(e).filter((e, v) => e[0].match(/spokesperson [0-9]/g) && e[1] !== 0)
                         if (spokesman.length !== 0) {
                             spokesman?.filter(async (s) => {
@@ -238,7 +238,7 @@ exports.saveArtical = async function (req, res, next) {
                                 }
                                 await articalService.createQaSpokesPerson(sperson).then(async (sps) => {
                                     const [sporkepeople, created] = sps;
-                                
+
                                     const spersondata = {
                                         spokesperson_id: sporkepeople.id,
                                         q_article_id: q_articles?.id,
@@ -257,7 +257,7 @@ exports.saveArtical = async function (req, res, next) {
                                 }
                                 await articalService.createQaClientProduct(product).then(async (pro) => {
                                     const [products, created] = pro;
-                                    
+
                                     const spersondata = {
                                         product_id: products.id,
                                         q_article_id: q_articles?.id
@@ -274,8 +274,8 @@ exports.saveArtical = async function (req, res, next) {
         }
 
     })]).then((values) => {
-        console.log('values',values); // [3, 1337, "foo"]
-      });
+        console.log('values', values); // [3, 1337, "foo"]
+    });
 
     res.json({ message: 'Artical upload processing', data: {} });
 }
@@ -350,51 +350,54 @@ exports.processExcel = function (req, res) {
     });
 }
 
-exports.addSetting = async function(req, res, next) {
+exports.addSetting = async function (req, res, next) {
     articalService.addSetting(req.body)
-       .then((setting) =>{
-           res.json({setting: setting, message: 'Setting added successful' })
-       })
-       .catch(next);
+        .then((setting) => {
+            res.json({ setting: setting, message: 'Setting added successful' })
+        })
+        .catch(next);
 }
 
-exports.getSetting = async function(req, res, next) {
-    const check = articalService.getQualitativeCheck(req.params.client_id)
-    articalService.getSetting(req.params.client_id)
-   .then(data => {
-       res.json({settings: data, qualitative: check > 0 ? true : false, message: "Client setting fetched successfully"});
-   })
-   .catch(next);
-}
-
-exports.getQualitativeCheck = async function(req, res, next) {
+exports.getSetting = async function (req, res, next) {
     articalService.getQualitativeCheck(req.params.client_id)
-   .then(data => {
-       res.json({qualitative: data > 0 ? true : false , message: "Client Qualitative fetched successfully"});
-   })
-   .catch(next);
+        .then(check => {
+            articalService.getSetting(req.params.client_id)
+                .then(data => {
+                    res.json({ settings: data, qualitative: check > 0 ? true : false, message: "Client setting fetched successfully" });
+                })
+                .catch(next);
+        })
+        .catch(next);
 }
 
-exports.getSettingAll = async function(req, res, next) {
+exports.getQualitativeCheck = async function (req, res, next) {
+    articalService.getQualitativeCheck(req.params.client_id)
+        .then(data => {
+            res.json({ qualitative: data > 0 ? true : false, message: "Client Qualitative fetched successfully" });
+        })
+        .catch(next);
+}
+
+exports.getSettingAll = async function (req, res, next) {
     articalService.getSettingAll()
-   .then(data => {
-       res.json({settings: data, message: "Client setting fetched successfully"});
-   })
-   .catch(next);
+        .then(data => {
+            res.json({ settings: data, message: "Client setting fetched successfully" });
+        })
+        .catch(next);
 }
 
-exports.updateSetting = async function(req, res, next) {
+exports.updateSetting = async function (req, res, next) {
     articalService.updateSetting(req.params.id, req.body)
-   .then(data => {
-       res.json({settings: {}, message: "Client setting updated successfully"});
-   })
-   .catch(next);
+        .then(data => {
+            res.json({ settings: {}, message: "Client setting updated successfully" });
+        })
+        .catch(next);
 }
 
-exports.deleteSetting = async function(req, res, next) {
+exports.deleteSetting = async function (req, res, next) {
     articalService.deleteSetting(req.params.id)
-   .then(data => {
-       res.json({settings: {}, message: "Client setting deleted successfully"});
-   })
-   .catch(next);
+        .then(data => {
+            res.json({ settings: {}, message: "Client setting deleted successfully" });
+        })
+        .catch(next);
 }
